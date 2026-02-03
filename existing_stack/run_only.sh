0d181c898a9a3e20865f92c9ab5e9c4e5b5922e2
Dean H Lorenz <dean@il.ibm.com>
2026-02-03 19:19:26 +0200

    Run_only handle disconnects (#636)
    * Use bash -c to allow test to keep running even if connection is lost.
    
    * Added text to explain async operation
    
    Signed-off-by: Dean H Lorenz <dean@il.ibm.com>
    
    ---------
    
    Signed-off-by: Dean H Lorenz <dean@il.ibm.com>
    Signed-off-by: maugustosilva <marcio.a.silva@ibm.com>
    Co-authored-by: maugustosilva <marcio.a.silva@ibm.com>

--- existing_stack/run_only.sh
+++ existing_stack/run_only.sh
@@ -73,6 +73,7 @@ function announce {
             echo -e "==> $(date) - ${0} - $message" >> ${logfile}
             ;;  
     esac
+
 }
 
 # Sanitize pod name to conform to Kubernetes naming conventions
@@ -398,6 +399,14 @@ set -e
 # Execute workloads
 # ========================================================
 set +e
+announce "ℹ️
+  Running benchmark with Experiment ID ${_uid}.
+  Results will be stored in PVC ${harness_results_pvc}.
+
+  Note: 
+    Benchmark will continue to run even on time-out or connection failure.
+    Can follow progress by checking the logs (${control_kubectl} logs -f ${_pod_name} -n ${harness_namespace}).
+"
 if [ "${harness_wait_timeout}" -eq 0 ]; then
   _timeout=""
 else
@@ -405,16 +414,17 @@ else
 fi
 yq '.workload | keys | .[]' "${_config_file}" |
   while IFS= read -r workload; do
-    announce "ℹ️ Running benchmark with workload ${workload}"
-    ${_timeout} $control_kubectl exec -i ${_pod_name} -- bash <<RUN_WORKLOAD
-# redirect to root fds so that kubectl logs can capture output
-exec 1> >(tee /proc/1/fd/1 >&1)
-exec 2> >(tee /proc/1/fd/2 >&2)
+    announce "ℹ️ Running benchmark with workload ${workload}."
+    IFS= read -r -d '' run_workload <<RUN_WORKLOAD
+      # redirect to root fds so that kubectl logs can capture output
+      exec 1> >(tee /proc/1/fd/1 >&1)
+      exec 2> >(tee /proc/1/fd/2 >&2)
 
-export LLMDBENCH_RUN_EXPERIMENT_ID="${_uid}_${workload}"
+      export LLMDBENCH_RUN_EXPERIMENT_ID="${_uid}_${workload}"
 
-${HARNESS_EXECUTABLE} --harness="${harness_name}" --workload="${workload}"
+      ${HARNESS_EXECUTABLE} --harness="${harness_name}" --workload="${workload}"
 RUN_WORKLOAD
+    ${_timeout} $control_kubectl exec -i ${_pod_name} -- bash -c "$run_workload"
     res=$?
     if [ $res -eq 0 ]; then
       announce "ℹ️ Benchmark workload ${workload} complete."
